655284e1ddf8b6338ad7764ce1c2453c7d67765d
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-11 00:17:18 +0400

    Add sketch div generation logic

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const sketch = document.querySelector('.container.sketch');
 let divCount = 0;
+
+function populateSketch(size) {
+    for (let i=0; i<size*size; i++) {
+        const div = document.createElement('div');
+        div.style.width = 600/(size) + 'px';
+        div.style.height = 400/(size) + 'px';
+        sketch.appendChild(div);
+    }
+}
 
 function chooseSizeBTN() {
     divCount = +prompt('Please choose even number from 1 to 64')
-    if ((divCount%2 === 0 && divCount < 64 && divCount > 1) || divCount === 1) {
-
+    if ((divCount%2 === 0 && divCount <= 64 && divCount > 1) || divCount === 1) {
+        populateSketch(divCount);
     } else {
         alert('Incorrect input. Please try again!');
     }
