1dfae8f2244b122d1238b3587899c5570c0dc8e2
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-10 23:17:56 +0400

    Add boilerplate for app logic

---
+++ app.js
@@ -0,0 +1,12 @@
+let divCount = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', e => {
+    if (e.target.innerText === 'Choose Size') {
+        
+    } else if (e.target.innerText === 'Reset') {
+
+    } else if (e.target.innerText === 'Erase') {
+
+    }
+}));
